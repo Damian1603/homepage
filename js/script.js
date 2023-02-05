2a661fd97bcb7a1da8159fc7f1c20d655372ca8c
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-02-05 11:12:15 +0100

    changed welcome message in the console

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
-console.log("Cześć wszystkim")
+console.log("Cześć wszystkim. Robię kolejne zmiany")
 
 let body = document.querySelector(".js-body--dark");
 let button = document.querySelector(".js-button");
-
-
-
 
 button.addEventListener("click", () => {
     button.innerText = button.innerText === "Jasny motyw" ? "Ciemny motyw" : "Jasny motyw";
 
     body.classList.toggle("js-body--dark");
-
 });
-  
-
