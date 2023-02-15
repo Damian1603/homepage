ec7868aeca05bf47957cc02594ba711157ad5e39
Damian Kuczynski <kuczynski.damian00@wp.pl>
2023-02-15 12:38:51 +0100

    add functions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-console.log("Cześć wszystkim. Robię kolejne zmiany")
-
-let body = document.querySelector(".js-body--dark");
-let button = document.querySelector(".js-button");
-
-button.addEventListener("click", () => {
-    button.innerText = button.innerText === "Jasny motyw" ? "Ciemny motyw" : "Jasny motyw";
-
-    body.classList.toggle("js-body--dark");
-});
+{
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-body");
+        const nextColorName = document.querySelector(".nextColorName");
+        body.classList.toggle("js-body--dark");
+        nextColorName.innerText = body.classList.contains("js-body--dark") ? "jasny" : "ciemny";
+    };
+    
+    const init = () => {
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", toggleBackground);
+    };
+    init();
+}
